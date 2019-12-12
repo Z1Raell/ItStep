764d0a207ebda7b7a53b0fd969265bd4696e72c0
Maks Lysenko <maksimka.lysenko2001@gmail.com>
2019-12-12 16:32:12 +0200

    05_12_2019

---
+++ homeWork1-05_12_2019/example.js
@@ -0,0 +1,65 @@
+//Задания 1 
+
+/*let name = prompt('Как тебя звоут')
+alert('Привет,' + name)*/
+
+//Задания 2
+
+/*const Year = 2019
+let ageYear = prompt('Ваш год рождения')
+
+alert('Вам ' + (ageYear - Year * 4) +' Лет ')*/
+
+//Задания 3
+
+/*let side = prompt('Задайте сторону квадрата')
+alert( side * 4)*/
+
+//Задания 4
+
+/*let radius = prompt('Введите радиус окружности')
+const pi = 3.14
+alert(radius * pi ** 2)*/
+
+//Задания 5
+
+/*let distance = prompt('Введите розстояние')
+
+let time = prompt('Введите время')
+
+alert(distance / time)*/
+
+//Задние 6
+
+/*let quantity = prompt('Введите количество')
+const dolar = 24
+let euro = 29
+alert(quantity*(dolar/euro))
+*/
+//Хадания 7 
+
+/*const mb = 0.820
+let space = prompt('Введите место на флэшке')
+alert(space/mb)*/
+
+//Задания 8
+
+
+/*let sum = prompt('Сумма денег');
+let price = prompt('Цена одной шоколадки');
+let number = (sum / price);
+alert(number);
+alert(sum - number);*/
+
+//Задания 9
+
+/*let number = prompt('Введите трёхзначное число')
+alert( number%1000 )*/
+
+//Задания 10 
+/*
+let number = prompt('Введите пожалуйста целое число');
+let even = (number == !(number % 2));
+alert(even)*/
+
+
