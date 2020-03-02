d27179170ec2575bc1b81e3d18f2daee5c45c488
Maks Lysenko <maksimka.lysenko2001@gmail.com>
2020-03-02 13:57:22 +0200

    14_01_2020

---
+++ homeWOrk14_01_2020/example.js
@@ -0,0 +1,68 @@
+'use strict'
+
+//Task 1 
+
+/*function sum() {
+    let result = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        result = result + arguments[i];
+    }
+    console.log(result);
+}
+
+sum(2, 5, 9, 12);*/
+
+//Task 2 
+
+/*function multiplicationTable(a) {
+    let result = 0;
+    for (var i = 0; i < 1; i++) {
+        for (var j = 1; j <= 9; j++) {
+            result = a * j;
+            console.log(result);
+        }
+    }
+
+}
+multiplicationTable(2);
+multiplicationTable(3);
+multiplicationTable(4);
+multiplicationTable(5);
+multiplicationTable(6);
+multiplicationTable(7);
+multiplicationTable(8);
+multiplicationTable(9);*/
+
+//Task 3 
+
+/*function pairedBrackets(x) {
+    let y = '('
+    let z = ')'
+    for (var i = 0; i < x; i++) {
+        console.log(y + z);
+    }
+}
+pairedBrackets(4);*/
+
+//Task 4 
+
+/*function division(x) {
+    for (let i = 0; x < 50; i++) {
+        let result = x / 2;
+        console.log(i);
+        console.log(result);
+    }
+}
+division(100);*/
+
+//Task 5
+
+/*function numberOfCalls() {
+    let count = 1;
+    return function() {
+       return  count++;
+    }
+}
+numberOfCalls();
+numberOfCalls();
+numberOfCalls();*/
